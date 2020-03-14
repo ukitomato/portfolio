2443a7e33f92e65134fb1965fff70bc215755f31
Yuki Yamato <ukitomato.developer@gmail.com>
2020-03-14 11:37:50 +0900

    test CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,12 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
                 <p>
-                    I will edit <code>src/App.tsx</code> and save to reload.
+                    Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
+                <a className="App-link"
+                   href="https://reactjs.org"
+                   target="_blank"
+                   rel="noopener noreferrer"
                 >
                     Learn React
                 </a>
