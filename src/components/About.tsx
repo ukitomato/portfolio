7be4652d330b8b1a19cf63539097084bd76215a7
Yuki Yamato <ukitomato.developer@gmail.com>
2020-04-07 23:58:37 +0900

    Designed Skill Page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import person from '../imgs/person.gif';
 import '../App.css';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Link } from 'react-scroll';
 import ColoredLine from './ColoredLine';
 
-function About() {
+function About(): ReactElement {
     return (
         <Container fluid id="about" className="about page">
-            <Row className="justify-content-md-center align-items-center">
-                <Col xs={6} md={5} lg={4}>
+            <Row className="justify-content-center">
+                <Col xs={10} md={10} lg={4}>
                     <img src={person} alt="placeholder" className="img-fluid" />
                 </Col>
-                <Col xs={6} md={5} lg={4}>
-                    <Col className="section-heading">
-                        <h2>
+                <Col xs={10} md={10} lg={4}>
+                    <Col className="section-heading justify-content-start">
+                        <h2 className={'text-left'}>
                             About <strong>Me</strong>
                         </h2>
                         <ColoredLine
@@ -22,31 +22,33 @@ function About() {
                             className={'section-line'}
                         />
                     </Col>
-                    <p className="description">
-                        I&apos;m a Master&apos;s student at University of
-                        Tsukuba <br /> and an engineer.
-                    </p>
-                    <p className="about-page">
-                        I&apos;m majoring in User Interface, Human-Computer
-                        Interaction, Computer Science. I have proposed
-                        interfaces that enable a new interaction and have
-                        implemented a prototype/Proof of Concept. As an
-                        engineer, I have experienced both a long-term internship
-                        and short-term internships. Of course, I also do
-                        developments on my own.
-                    </p>
-                    <Button variant="light" size={'lg'} className={'mt-5'}>
-                        <Link
-                            activeClass="active"
-                            to={'contact'}
-                            spy={true}
-                            smooth={true}
-                            offset={-56}
-                            duration={500}
-                        >
-                            Contact Me
-                        </Link>
-                    </Button>
+                    <Col className="justify-content-start">
+                        <p className="description text-left">
+                            I&apos;m a Master&apos;s student at University of
+                            Tsukuba <br /> and an engineer.
+                        </p>
+                        <p className="about-page text-left">
+                            I&apos;m majoring in User Interface, Human-Computer
+                            Interaction, Computer Science. I have proposed
+                            interfaces that enable a new interaction and have
+                            implemented a prototype/Proof of Concept. As an
+                            engineer, I have experienced both a long-term
+                            internship and short-term internships. Of course, I
+                            also do developments on my own.
+                        </p>
+                        <Button variant="light" size={'lg'} className={'mt-5'}>
+                            <Link
+                                activeClass="active"
+                                to={'contact'}
+                                spy={true}
+                                smooth={true}
+                                offset={-56}
+                                duration={500}
+                            >
+                                Contact Me
+                            </Link>
+                        </Button>
+                    </Col>
                 </Col>
             </Row>
         </Container>
--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -1,24 +1,61 @@
-import React from 'react';
-import logo from '../logo.svg';
+import React, { ReactElement } from 'react';
 import '../App.css';
-import { Button, Container } from 'react-bootstrap';
+import { Col, Container, ProgressBar, Row } from 'react-bootstrap';
+import ColoredLine from './ColoredLine';
 
-function Skill() {
+const java = 85;
+const android = 70;
+const python = 65;
+const nodejs = 70;
+const react = 70;
+const git = 70;
+
+function Skill(): ReactElement {
     return (
-        <Container fluid id="skill" className="page">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>Skill</p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </a>
-            <Button variant="primary" className="mr-1">
-                Primary
-            </Button>
+        <Container fluid id="skill" className="page ">
+            <Row className="justify-content-center">
+                <Col
+                    xs={4}
+                    md={4}
+                    lg={4}
+                    className="section-heading  justify-content-star"
+                >
+                    <h2 className={'text-left'}>
+                        My
+                        <br />
+                        Programming
+                        <br />
+                        <strong>Skill</strong>
+                    </h2>
+                    <ColoredLine color={'#52cef6'} className={'section-line'} />
+                </Col>
+                <Col xs={6} md={6} lg={7} className={'skill-progress'}>
+                    <Row>
+                        <h5>Java</h5>
+                        <ProgressBar now={java} label={`${java}%`} />
+                    </Row>
+                    <Row>
+                        <h5>Android</h5>
+                        <ProgressBar now={android} label={`${android}%`} />
+                    </Row>
+                    <Row>
+                        <h5>Python</h5>
+                        <ProgressBar now={python} label={`${python}%`} />
+                    </Row>
+                    <Row>
+                        <h5>NodeJS</h5>
+                        <ProgressBar now={nodejs} label={`${nodejs}%`} />
+                    </Row>
+                    <Row>
+                        <h5>React</h5>
+                        <ProgressBar now={react} label={`${react}%`} />
+                    </Row>
+                    <Row>
+                        <h5> Git</h5>
+                        <ProgressBar now={git} label={`${git}%`} />
+                    </Row>
+                </Col>
+            </Row>
         </Container>
     );
 }
