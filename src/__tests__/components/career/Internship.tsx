1a6ba025e7a9cf84ae4477b7407aa7babc1a4389
Yuki Yamato <ukitomato.developer@gmail.com>
2020-04-08 12:55:38 +0900

    Designed Career layout

---
+++ src/__tests__/components/career/Internship.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import gluegent from '../../../imgs/gluegent.gif';
+import Internship from '../../../components/career/Internship';
+
+it('should render without crashing', () => {
+    shallow(
+        <Internship
+            name={'Test'}
+            topic={'Test'}
+            term={'Test'}
+            type={'Test'}
+            position={'Test'}
+            description={'Test'}
+            img={gluegent}
+        />
+    );
+});
