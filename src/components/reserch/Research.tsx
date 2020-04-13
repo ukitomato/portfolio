94b2e02966ab374a061d7a87994e41a2ed42a112
Yuki Yamato <ukitomato.developer@gmail.com>
2020-04-14 00:32:41 +0900

    Designed Research Layout

---
+++ src/components/reserch/Research.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+
+const Research = (props: {
+    name: string;
+    img: string;
+    description: string;
+    type: string;
+    term: string;
+    topic: string;
+    publish: string;
+    url: string;
+}) => {
+    return (
+        <Card bg={'dark'}>
+            <Card.Header as={'h3'}>{props.name}</Card.Header>
+            <Card.Body>
+                <Row className="justify-content-center align-items-center">
+                    <Col xs={10} md={10} lg={8}>
+                        <Col className="justify-content-start">
+                            <p className="r-description text-left">
+                                {props.description}
+                            </p>
+                        </Col>
+                        <Col>
+                            <ul className="site-list">
+                                <li className="site-check">
+                                    Type: {props.type}
+                                </li>
+                                <li className="site-check">
+                                    Term:{' '}
+                                    <span className="date">
+                                        {props.term}
+                                    </span>
+                                </li>
+                                <li className="site-check">
+                                    Topic: {props.topic}
+                                </li>
+                            </ul>
+                        </Col>
+                        <Col className="justify-content-start">
+                            <p className="r-publish text-left">
+                                {props.publish}
+                            </p>
+                        </Col>
+                    </Col>
+                    <Col xs={10} md={10} lg={2}>
+                        <a href={props.url}>
+                        <img
+                            src={props.img}
+                            alt="placeholder"
+                            className="img-fluid"
+                        />
+                        </a>
+                    </Col>
+
+                </Row>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default Research;
