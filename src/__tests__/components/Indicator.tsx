6ec4acf718e2e73652ce9a2e74a10d369062c3a0
Yuki Yamato <ukitomato.developer@gmail.com>
2021-04-05 15:39:56 +0900

    Update test codes

---
+++ src/__tests__/components/Indicator.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Indicator from '../../components/Indicator';
+
+test('renders App', () => {
+    render(<Indicator />);
+});
